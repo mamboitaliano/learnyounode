fe8b716c11b7f49ab1cca08c4f9a4e0c32637499
raidersofthenight <ianemailaddress@gmail.com>
2015-08-29 17:46:30 -0700

    require fs

--- fileserver.js
+++ fileserver.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var fs = require('fs');
 var port = process.argv[2];
 var textFile = process.argv[3];
 
 console.log(port);
 console.log(textFile);
+console.log(fs.createReadStream(textFile));
