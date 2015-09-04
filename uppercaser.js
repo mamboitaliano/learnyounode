1a2d7efb23407229de45a5688ce3fe732b40c2c1
raidersofthenight <ianemailaddress@gmail.com>
2015-09-03 19:14:16 -0700

    complete HTTP uppercaser challenge

--- uppercaser.js
+++ uppercaser.js
@@ -4,40 +4,15 @@ var port = process.argv[2];
 
 var server = http.createServer(function(req, res) {
 	if (req.method ==='POST') {
-
-		var body = '';
-		req.setEncoding('utf8');
-
-		req.on('data', function (chunk) {
-			console.log('chunk time!');
-			body += chunk;
-			console.log(body);
-		});
-
-		req.on('end', function() {
-			try {
-				var data = JSON.parse(body);
-			}
-			catch (er) {
-				// in case of bad json
-				res.statusCode = 400;
-				return res.end('error: ' + er.message);
-			}
-
-			res.write(typeof data);
-			res.end();
-		});
-
-		// req.pipe(map(function (chunk) {
-		// 	console.log(chunk.toString());
-		// })).pipe(res);
+		req.pipe(map(function (chunk) {
+			return (chunk.toString().toUpperCase());
+		})).pipe(res);
+		res.writeHead(200, {'content-type': 'text/plain'});
 	}
 	else {
-		console.log("no post data");
+		console.log("no post data\n");
 		res.end();
 	}
-
-	res.writeHead(200, {'content-type': 'text/plain'});
 });
 
 server.listen(port);
